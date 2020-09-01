b038035a7cbaac51474af4b020f917ec6f6077a0
smitcs19 <smitcs19@wfu.edu>
2020-09-01 16:20:43 -0400

    Error resolved

--- src/Exception.java
+++ src/Exception.java
@@ -5,6 +5,11 @@ public class Exception {
         int[] arr = new int[]{0,1,2,3,4,5};
         System.out.println("Enter the index of the array to print its content");
         Scanner index = new Scanner(System.in);
-        System.out.println(arr[index.nextInt()]);
+
+        try {
+            System.out.println(arr[index.nextInt()]);
+        } catch(IndexOutOfBoundsException e1){
+            System.out.println("Problem with code detected");
+        }
     }
 }
